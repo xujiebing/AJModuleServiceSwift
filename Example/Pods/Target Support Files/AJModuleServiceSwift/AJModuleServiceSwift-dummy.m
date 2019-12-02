2c599ae9cad8c425499f4b1497205fd1c5d6cf55
xujiebing <xujiebing1992@gmail.com>
2019-12-02 23:51:16 +0800

    初始化

---
+++ Example/Pods/Target Support Files/AJModuleServiceSwift/AJModuleServiceSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AJModuleServiceSwift : NSObject
+@end
+@implementation PodsDummy_AJModuleServiceSwift
+@end
---
+++ Example/Pods/Target Support Files/Pods-AJModuleServiceSwift_Tests/Pods-AJModuleServiceSwift_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AJModuleServiceSwift_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AJModuleServiceSwift_Tests
+@end
