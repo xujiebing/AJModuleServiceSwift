b1bd6a9f11004229ba82cf18b25ec25bbae19df8
shanying <shanying@icheego.com>
2023-01-05 09:56:52 +0800

    需求开发

--- AJModuleServiceSwift/Classes/AJAppDelegate.swift
+++ AJModuleServiceSwift/Classes/AJAppDelegate.swift
@@ -7,66 +7,66 @@
 
 import UIKit
 
-@main
-class AJAppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-    required override init() {
-        AJModuleService.register()
-    }
-    
-    func applicationDidFinishLaunching(_ application: UIApplication) {
-        let array = AJModuleServiceManager.shared.serviceArray
-        for service in array {
-            if service.responds(to: #selector(applicationDidFinishLaunching(_:))) {
-                service.applicationDidFinishLaunching(application)
-            }
-        }
-    }
-     
-    func applicationWillResignActive(_ application: UIApplication) {
-        let array = AJModuleServiceManager.shared.serviceArray
-        for service in array {
-            if service.responds(to: #selector(applicationWillResignActive(_:))) {
-                service.applicationWillResignActive(application)
-            }
-        }
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        let array = AJModuleServiceManager.shared.serviceArray
-        for service in array {
-            if service.responds(to: #selector(applicationDidEnterBackground(_:))) {
-                service.applicationDidEnterBackground(application)
-            }
-        }
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        let array = AJModuleServiceManager.shared.serviceArray
-        for service in array {
-            if service.responds(to: #selector(applicationWillEnterForeground(_:))) {
-                service.applicationWillEnterForeground(application)
-            }
-        }
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        let array = AJModuleServiceManager.shared.serviceArray
-        for service in array {
-            if service.responds(to: #selector(applicationDidBecomeActive(_:))) {
-                service.applicationDidBecomeActive(application)
-            }
-        }
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        let array = AJModuleServiceManager.shared.serviceArray
-        for service in array {
-            if service.responds(to: #selector(applicationWillTerminate(_:))) {
-                service.applicationWillTerminate(application)
-            }
-        }
-    }
-
-}
+//@main
+//class AJAppDelegate: UIResponder, UIApplicationDelegate {
+//
+//    var window: UIWindow?
+//    required override init() {
+//        AJModuleService.register()
+//    }
+//
+//    func applicationDidFinishLaunching(_ application: UIApplication) {
+//        let array = AJModuleServiceManager.shared.serviceArray
+//        for service in array {
+//            if service.responds(to: #selector(applicationDidFinishLaunching(_:))) {
+//                service.applicationDidFinishLaunching(application)
+//            }
+//        }
+//    }
+//
+//    func applicationWillResignActive(_ application: UIApplication) {
+//        let array = AJModuleServiceManager.shared.serviceArray
+//        for service in array {
+//            if service.responds(to: #selector(applicationWillResignActive(_:))) {
+//                service.applicationWillResignActive(application)
+//            }
+//        }
+//    }
+//
+//    func applicationDidEnterBackground(_ application: UIApplication) {
+//        let array = AJModuleServiceManager.shared.serviceArray
+//        for service in array {
+//            if service.responds(to: #selector(applicationDidEnterBackground(_:))) {
+//                service.applicationDidEnterBackground(application)
+//            }
+//        }
+//    }
+//
+//    func applicationWillEnterForeground(_ application: UIApplication) {
+//        let array = AJModuleServiceManager.shared.serviceArray
+//        for service in array {
+//            if service.responds(to: #selector(applicationWillEnterForeground(_:))) {
+//                service.applicationWillEnterForeground(application)
+//            }
+//        }
+//    }
+//
+//    func applicationDidBecomeActive(_ application: UIApplication) {
+//        let array = AJModuleServiceManager.shared.serviceArray
+//        for service in array {
+//            if service.responds(to: #selector(applicationDidBecomeActive(_:))) {
+//                service.applicationDidBecomeActive(application)
+//            }
+//        }
+//    }
+//
+//    func applicationWillTerminate(_ application: UIApplication) {
+//        let array = AJModuleServiceManager.shared.serviceArray
+//        for service in array {
+//            if service.responds(to: #selector(applicationWillTerminate(_:))) {
+//                service.applicationWillTerminate(application)
+//            }
+//        }
+//    }
+//
+//}
